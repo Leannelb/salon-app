445d09547a72659c3e63fd2e7f09c3566795bb9f
Leannelb <leannelaceybyrne@outlook.com>
2025-03-09 13:51:03 +0000

    fix: adding correct titles to pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,44 @@
 import { Stack } from "expo-router";
+import { PaperProvider } from 'react-native-paper';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      {/* <Stack.Screen name="index" options={{ title: 'Home' }} />
-      <Stack.Screen name="about" options={{ title: 'About' }} /> */}
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen 
-        name="screens/stylist-selection" 
-        options={{ 
-          title: 'Select Stylist',
-          headerShown: true 
-        }} 
-      />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <PaperProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen 
+          name="screens/stylist-selection" 
+          options={{ 
+            title: 'Select Stylist',
+            headerShown: true 
+          }} 
+        />
+        <Stack.Screen 
+          name="screens/branch-selection" 
+          options={{ 
+            title: 'Select Your Branch',
+            headerShown: true 
+          }} 
+        />
+        <Stack.Screen 
+          name="screens/date-time-selection" 
+          options={{ 
+            title: 'Select Appointment Date and Time',
+            headerShown: true 
+          }} 
+        />
+        <Stack.Screen name="booking-management" options={{ title: "My Appointments" }} />
+        <Stack.Screen name="screens/booking-screen" options={{ title: "Create a New Appointment" }} />
+        <Stack.Screen name="screens/booking-complete" options={{ title: "Appointment Confirmation" }} />
+        <Stack.Screen name="screens/booking-management" options={{ title: "My Appointments" }} />
+        <Stack.Screen name="screens/reschedule-assistant" options={{ title: "Rescheduling Assistant" }} />
+
+        
+        
+
+        <Stack.Screen name="reschedule-assistant" options={{ title: "Reschedule" }} />      
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </PaperProvider>
   )
 }
